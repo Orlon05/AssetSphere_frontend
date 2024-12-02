0fbe2ee5451c70b451d67763d3f9717e8a69b711
Wilson Chavarria Herrero <2797443@tcs.com>
2024-12-02 17:50:00 -0500

    Limpiar estilos para permitir el desplazamiento

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
-import style from './dashboard.module.css'
+import style from "./dashboard.module.css";
 import Sidebar from "../layouts/Sidebar";
 import Navegacion from "../layouts/Navegacion";
 
 const Dashboard = () => {
+  const [abrirMenu, cerrarMenu] = useState(false);
 
-    const[abrirMenu, cerrarMenu] = useState(false);
+  const toggleSidebar = () => {
+    //Esta constante es, para pasar el hook luego al crear  el menu lateral
+    cerrarMenu(!abrirMenu);
+  };
 
-    const toggleSidebar = () => {//Esta constante es, para pasar el hook luego al crear  el menu lateral
-        cerrarMenu(!abrirMenu);
-    };
-
-    return (
-        <main>
-            <Navegacion toggleSidebar={toggleSidebar} />
-            <section className={style.panelControl}>
-            < Sidebar isOpen={abrirMenu} />
-                <section>
-                    <Outlet />
-                </section>
-            </section>
-        </main>
-    );
+  return (
+    <main>
+      <section className={style.panelControl}>
+        <Sidebar isOpen={abrirMenu} />
+        <section>
+          <Navegacion toggleSidebar={toggleSidebar} />
+          <Outlet />
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default Dashboard;
