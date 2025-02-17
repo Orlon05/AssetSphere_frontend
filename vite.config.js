ef5d961d360ec6e8aca479690aa809975d942cc6
Juan Jose Quintero Reatiga <jujquint@bancolombia.com.co>
2025-02-17 15:52:50 -0500

    Despliegue de cambios de url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/assets",
+          dest: "",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    // assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  base: "/inveplus/",
+  server: {
+    hmr: {
+      port: 443,
+    },
+  },
+});
