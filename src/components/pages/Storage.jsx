74c073697e437bf90e6932b8e06c0cc045ce6d21
Jose Paez Holguin <2892217@tcs.com>
2025-03-11 15:12:43 -0500

    Se realiza corrección de campos desplegables, dejándolos solo con input en los módulos de Pseries, Base de Datos y Sucursales, dejando pendiente Storage a la espera de los cambios para importar y exportar datos en Excel

--- src/components/pages/Storage.jsx
+++ src/components/pages/Storage.jsx
@@ -126,7 +126,7 @@ const Storage = () => {
       }
       // Se deben colocar todas las propiedades/campos de la tabla a la cual se le este haciendo la vista a excepción de la clave prinaria, dicho campo no debe ir aquí
       const formattedData = importedData.map(row => ({
-        cod_item_configuracion: row.cod_item_configuracion || "",
+        cod_item_configuracion: String(row.cod_item_configuracion || ""),
         name: row.name || "",
         application_code: row.application_code || "",
         cost_center: row.cost_center || "",
