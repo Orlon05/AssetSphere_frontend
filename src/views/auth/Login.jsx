b1ce883cdb71a95cd690b17efd696eac81617cee
SebasxSwat <sebastianescalante2006@gmail.com>
2025-06-03 14:28:30 -0500

    Api

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -34,11 +34,14 @@ const Login = () => {
     setErrorMessage("");
 
     try {
-      const response = await fetch("https://10.8.150.90/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+      const response = await fetch(
+        "https://10.8.150.90/api/inveplus/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
       const data = await response.json();
 
