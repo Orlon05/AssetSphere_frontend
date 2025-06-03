6eaec1784198cae5af7481e633e69439a5db4117
caararbo <caararbo@bancolombia.com.co>
2025-06-03 10:04:28 -0500

    Correción URL

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
     setErrorMessage("");
 
     try {
-      const response = await fetch("http://10.8.150.90:443/auth/login", {
+      const response = await fetch("https://10.8.150.90/inveplus/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
